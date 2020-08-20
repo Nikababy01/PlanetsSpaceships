536bb427d3765584569b67a1922b34e39bcdf003
Monique.Bass01 <monique.bass01@gmail.com>
2020-08-20 17:29:17 -0500

    updated dictionary list

--- PlanetsSpaceships/Program.cs
+++ PlanetsSpaceships/Program.cs
@@ -25,15 +25,40 @@ namespace PlanetsSpaceships
             Console.WriteLine($"There are 4 rocky planets and they are: {string.Join(", ", rockyPlanets)}.");
 
             Console.WriteLine("============================================================================");
-            //Iterating over planets
+            //Iterating over the ships first, then over then the planets
 
-            var spacecraftNames = new Dictionary<string,List<string>> ();
+            var spacecraftNames = new Dictionary<string, List<string>>
             {
                 {
-                    "Spacecraft1",
-                        new List<String> { "Earth", "Mars", "Mercury" };
+                    "Spacecraft",
+                        new List<string> { "Earth", "Mars", "Mercury"}
                 }
             };
+            foreach (var (ship, definitions) in spacecraftNames)
+            {
+                Console.WriteLine($"The {ship} has traveled to planets.");
+                foreach (var definition in definitions)
+                {
+                    Console.WriteLine($" {definition}");
+                }
+            }
+            //1. first create the first dictionary list
+            var rockyplanets = new Dictionary<string, List<string>>();
+            //2. next create a list of spacecrafts
+            var rockyPlanetShips = new List<string> { "Rocky XX1", "Rocky IV" };
+            var nonrockyPlanetShips = new List<string> { "FireBox II", "FireStarter III" };
+            //3. Then add to that list using Add. passing in a string and a dictionary list
+            rockyplanets.Add("Mercury", rockyPlanetShips);
+            rockyplanets.Add("Venus", rockyPlanetShips);
+            rockyplanets.Add("Jupiter", nonrockyPlanetShips);
+            rockyplanets.Add("Neptune", nonrockyPlanetShips);
+
+            //4. foreach over and use key and value
+
+            foreach (var planet in rockyplanets)
+            {
+                Console.WriteLine($"The planet {planet.Key} has been visited by these spaceships: {string.Join(", ", planet.Value)}.");
+            }
         }
     }
 }
